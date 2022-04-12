42127948656e3433320deb64695bffba3fd9e9ec
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-04-12 21:59:19 +0530

    first commit

---
+++ User1First.java
@@ -0,0 +1,5 @@
+public class User1First {
+    public static void main(String[] args) {
+        System.out.println("Hello User 1, I am program 1!");
+    }
+}
---
+++ User1Second.java
@@ -0,0 +1,5 @@
+public class User1Second {
+    public static void main(String[] args) {
+        System.out.println("Hello World, I am program 2!!!");
+    }
+}
