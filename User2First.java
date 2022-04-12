ce6decd7801df62400d60b97a467224a9309cca9
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-04-12 22:29:32 +0530

    New files added by user 2

---
+++ User2First.java
@@ -0,0 +1,5 @@
+public class User2First {
+    public static void main(String[] args) {
+        System.out.print("Hello World, I am program 1!!! (user 2)\n");
+    }
+}
---
+++ User2Second.java
@@ -0,0 +1,5 @@
+public class User2Second {
+    public static void main(String[] args) {
+        System.out.print("Hello World, I am program 2!!! (User 2)\n");
+    }
+}
