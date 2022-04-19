dd94d36b2e518df4323cf3f33511b2573c364f1a
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-04-19 11:51:01 +0530

    File updated by Dev 1, will result in conflict

--- User2First.java
+++ User2First.java
@@ -1,5 +1,6 @@
 public class User2First {
     public static void main(String[] args) {
         System.out.print("Hello World, I am program 1!!! (user 2)\n");
+        System.out.print("Changes by user 1 that would result in conflict");
     }
 }
