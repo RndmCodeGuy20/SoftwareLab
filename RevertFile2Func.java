33cafd93834bd6d77adc8821c2fa897ff57ede67
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-04-26 10:23:41 +0530

    scond function added (will be deprecated)

--- RevertFile2Func.java
+++ RevertFile2Func.java
@@ -4,9 +4,14 @@ public class RevertFile2Func {
         System.out.println("Hello!!!");
     }
 
+    public void sayBye() {
+        System.out.println("Bye!!! (this function will be reverted!");
+    }
+
     public static void main(String[] args) {
         RevertFile2Func rFunc = new RevertFile2Func();
 
         rFunc.sayHello();
+        rFunc.sayBye();
     }
 }
