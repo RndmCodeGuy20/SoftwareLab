00e068437d0e7ece2ba2e9fb24634473a40b0732
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-04-26 09:34:28 +0530

    Second function added : master

--- TestFunc.java
+++ TestFunc.java
@@ -3,9 +3,14 @@ public class TestFunc {
         System.out.println("Hello!!!");
     }
 
+    public void sayBye() {
+        System.out.println("Bye!!!");
+    }
+
     public static void main(String[] args) {
         TestFunc tFunc = new TestFunc();
 
         tFunc.sayHello();
+        tFunc.sayBye();
     }
 }
