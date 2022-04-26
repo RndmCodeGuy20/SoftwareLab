184479bfa382118f1d125883257af58aabaa277f
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-04-26 10:22:13 +0530

    First Function Added

---
+++ RevertFile2Func.java
@@ -0,0 +1,12 @@
+public class RevertFile2Func {
+
+    public void sayHello() {
+        System.out.println("Hello!!!");
+    }
+
+    public static void main(String[] args) {
+        RevertFile2Func rFunc = new RevertFile2Func();
+
+        rFunc.sayHello();
+    }
+}
