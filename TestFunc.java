1e761d5556cc52843911e4f65726989a582eba51
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2022-04-26 09:32:06 +0530

    First function added : master

---
+++ TestFunc.java
@@ -0,0 +1,11 @@
+public class TestFunc {
+    public void sayHello() {
+        System.out.println("Hello!!!");
+    }
+
+    public static void main(String[] args) {
+        TestFunc tFunc = new TestFunc();
+
+        tFunc.sayHello();
+    }
+}
